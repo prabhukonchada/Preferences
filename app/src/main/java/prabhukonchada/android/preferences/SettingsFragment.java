f5a48386e140848eeb42a76333c3818fbc3074a6
prabhukonchada <prabhukonchada@gmail.com>
2017-10-06 08:29:07 +0530

    Added settings fragment and preferences resource file

--- app/src/main/java/prabhukonchada/android/preferences/SettingsFragment.java
+++ app/src/main/java/prabhukonchada/android/preferences/SettingsFragment.java
@@ -1,21 +1,12 @@
 package prabhukonchada.android.preferences;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.preference.PreferenceFragmentCompat;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
